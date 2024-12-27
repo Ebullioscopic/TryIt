ea70a08dfd3916e6ae6da231d57d40ec24f7a109
Ebullioscopic <hrhn.mudaliar251@gmail.com>
2024-12-27 19:09:50 +0530

    Initial Commit

---
+++ TryIt/TryItApp.swift
@@ -0,0 +1,17 @@
+//
+//  TryItApp.swift
+//  TryIt
+//
+//  Created by admin63 on 27/12/24.
+//
+
+import SwiftUI
+
+@main
+struct TryItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
